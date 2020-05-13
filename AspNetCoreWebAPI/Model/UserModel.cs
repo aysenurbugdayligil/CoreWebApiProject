95a68bd7070ad8ba8ce0faa21fdce57de21b1e6e
aysenurbugdayligil <aysenurbugdayligil@qlteq.com>
2020-05-13 16:03:08 +0300

    first commit

---
+++ AspNetCoreWebAPI/Model/UserModel.cs
@@ -0,0 +1,13 @@
+namespace AspNetCoreWebAPI.Model {
+    public class User {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+
+    public class UserRequestModel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
